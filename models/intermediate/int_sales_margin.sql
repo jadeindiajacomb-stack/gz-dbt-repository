9ad55b22634833738a0410961122ca8484261cb7
Jade Jacomb <jadeindiajacomb@gmail.com>
2025-10-29 16:23:25 +0000

    CalculateD the Margin per Order

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,3 +4,4 @@ sales.revenue -  (sales.quantity * product.purchase_price) AS margin
 FROM {{ ref('stg_raw__sales') }} sales
 LEFT JOIN {{ ref('stg_raw__product') }} product
 USING(products_id)
+WHERE orders_id = 1002561
